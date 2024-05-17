6a99fcb88a336ef3c41288d17fd6071ed11f7d56
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-17 09:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let fn = document.getElementById("fName");
+let ln = document.getElementById("lName");
+let pn = document.getElementById("pNumber");
+let eid = document.getElementById("eId");
+
+let btn = document.getElementById("btn");
+
+btn.onclick = ()=>{
+    alert(`${fn.name}: ${fn.value} ${ln.name}: ${ln.value} ${pn.name}: ${pn.value} ${eid.name}: ${eid.value}`)
+}
